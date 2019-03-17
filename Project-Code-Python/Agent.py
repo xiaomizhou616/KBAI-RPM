3c52f866d6d79eee110a1502def25ebf1d02a042
Xi Han <xhan306@gatech.edu>
2019-03-17 11:52:36 +0800

    Initial

---
+++ Project-Code-Python/Agent.py
@@ -0,0 +1,34 @@
+# Your Agent for solving Raven's Progressive Matrices. You MUST modify this file.
+#
+# You may also create and submit new files in addition to modifying this file.
+#
+# Make sure your file retains methods with the signatures:
+# def __init__(self)
+# def Solve(self,problem)
+#
+# These methods will be necessary for the project's main method to run.
+
+# Install Pillow and uncomment this line to access image processing.
+#from PIL import Image
+#import numpy
+
+class Agent:
+    # The default constructor for your Agent. Make sure to execute any
+    # processing necessary before your Agent starts solving problems here.
+    #
+    # Do not add any variables to this signature; they will not be used by
+    # main().
+    def __init__(self):
+        pass
+
+    # The primary method for solving incoming Raven's Progressive Matrices.
+    # For each problem, your Agent's Solve() method will be called. At the
+    # conclusion of Solve(), your Agent should return an int representing its
+    # answer to the question: 1, 2, 3, 4, 5, or 6. Strings of these ints 
+    # are also the Names of the individual RavensFigures, obtained through
+    # RavensFigure.getName(). Return a negative number to skip a problem.
+    #
+    # Make sure to return your answer *as an integer* at the end of Solve().
+    # Returning your answer as a string may cause your program to crash.
+    def Solve(self,problem):
+        return -1
---
+++ README.md
@@ -0,0 +1,1 @@
+# kbai-package-python
